ed7f365da74d50773ecc9ddffb7d879b5994efbc
liyongcode <1669844851@qq.com>
2018-05-17 17:43:06 +0800

    添加element-ui组件,vue-resource插件,vuex状态管理插件

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import VueResource from 'vue-resource'
+import Vuex from 'vuex'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+/*使用ElementUI插件*/
+Vue.use(ElementUI);
+/*使用VueResource插件*/
+Vue.use(VueResource);
+/*使用Vuex插件*/
+Vue.use(Vuex);
 
 /* eslint-disable no-new */
 new Vue({
